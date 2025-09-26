31eec756ad3bca1c52348b984e8fd1d25e4b8413
nailah <nailah24khaira@gmail.com>
2025-09-26 22:03:46 +0700

    pesan commit

--- app/src/main/java/com/example/shoppinglist/ui/theme/Color.kt
+++ app/src/main/java/com/example/shoppinglist/ui/theme/Color.kt
@@ -2,10 +2,29 @@ package com.example.shoppinglist.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light Mode
+val TealLight = Color(0xFF009688)
+val TealDark = Color(0xFF00796B)
+val TealOn = Color(0xFFFFFFFF)
+val TealBackground = Color(0xFFE0F2F1)
+val TealOnBackground = Color(0xFF1C1B1F)
+val TealSurface = Color(0xFFFFFFFF)
+val TealOnSurface = Color(0xFF1C1B1F)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val TealSurfaceDark = Color(0xFF2B2F2F)
+val TealOnSurfaceDark = Color(0xFFE6E1E5)
+
+val TealSecondary = Color(0xFF4DB6AC)
+val TealSecondaryContainer = Color(0xFFB2DFDB)
+val TealOnSecondaryContainer = Color(0xFF004D40)
+
+val TealPrimaryContainer = Color(0xFFB2DFDB)
+val TealOnPrimaryContainer = Color(0xFF004D40)
+
+// Dark Mode
+val TealDarkBackground = Color(0xFF121212)
+val TealDarkOnBackground = Color(0xFFE0F2F1)
+val TealDarkPrimaryContainer = Color(0xFF004D40)
+val TealDarkOnPrimaryContainer = Color(0xFFB2DFDB)
+val TealDarkSecondaryContainer = Color(0xFF00695C)
+val TealDarkOnSecondaryContainer = Color(0xFFB2DFDB)
